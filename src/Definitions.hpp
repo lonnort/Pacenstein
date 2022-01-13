ac132c82630d2653f05934f1d272fa5671fcd7d2
JarnoBrocker <jarno.brocker@gmail.com>
2022-01-13 15:45:56 +0100

    changed Defines

--- src/Definitions.hpp
+++ src/Definitions.hpp
@@ -1,4 +1,4 @@
 #pragma once
 
 #define SPLASH_BACKGROUND_FILEPATH "res/splash/background.png"
-#define BLINKY_MIDDEL_ONE_FILEPATH "res/sprites/ghosts/ghost_blinky_middle_one.png"
+#define GHOSTS_FILEPATH "res/sprites/ghosts/"
