079c437c7585d03a23234a342bea9a941a67ebec
Lennard Duinkerken <lennarduinkerken@gmail.com>
2022-01-17 12:13:59 +0100

    Add keys to definitions

--- src/Definitions.hpp
+++ src/Definitions.hpp
@@ -1,4 +1,17 @@
 #pragma once
 
+// File paths
 #define SPLASH_BACKGROUND_FILEPATH "res/splash/background.png"
 #define GHOSTS_FILEPATH "res/sprites/ghosts/"
+
+// Keyboard input keys
+#define KEY_UP        W
+#define KEY_DOWN      S
+#define KEY_LEFT      A
+#define KEY_RIGHT     D
+#define KEY_ALT_UP    Up
+#define KEY_ALT_DOWN  Down
+#define KEY_ALT_LEFT  Left
+#define KEY_ALT_RIGHT Right
+#define KEY_PAUSE     Escape
+#define KEY_EXIT      Delete
