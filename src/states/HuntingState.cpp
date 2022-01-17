cbf8c9915abd3d3232e04b1ad0a8a1aa6cbb6535
JarnoBrocker <jarno.brocker@gmail.com>
2022-01-17 15:49:02 +0100

    added more exit key

--- src/states/HuntingState.cpp
+++ src/states/HuntingState.cpp
@@ -38,6 +38,9 @@ namespace Pacenstein
 			{
 				std::cout << "Go To Game Screen" << std::endl;
 			}*/
+			if (sf::Event::KeyPressed == event.type) {
+				if (sf::Keyboard::Key::KEY_EXIT == event.key.code) this->data->window.close();
+			}
 		}
 	}
 
