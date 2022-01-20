bf401152fb298e462618ef2151bedab499aceef5
Appenzeill <daanroth@protonmail.com>
2022-01-20 13:35:00 +0100

    Raycasting werkt, maar ergens zit er een fout in het geheugen

--- src/states/InGameState.cpp
+++ src/states/InGameState.cpp
@@ -13,13 +13,16 @@ namespace Pacenstein {
     InGameState::InGameState(game_data_ref_t data) : data(data) {}
     
     void InGameState::init() {
-	this->data->assets.loadVertex("Vertex Top", sf::Vector2f(100,0),   sf::Color::Red);
-	this->data->assets.loadVertex("Vertex Bottom", sf::Vector2f(100,500),  sf::Color::Blue);
-	sf::Vertex vertices[4] = {
-	    this->data->assets.getVertex("Vertex Top"),
-	    this->data->assets.getVertex("Vertex Bottom")
-	};
+	this->posX = 4;
+	this->posY = 4;
 
+	this->dirX = -1;
+	this->dirY = 0;
+
+	this->planeX = 0, this->planeY = 0.66;
+	this->oldPlaneX = 0; this->oldPlaneY = 0.66;
+	this->time = 0, this->oldTime = 0;
+	
 	int worldMap[21][19]= {
 	    {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
 	    {1,4,4,4,4,4,4,4,4,1,4,4,4,4,4,4,4,4,1},
@@ -68,45 +71,88 @@ namespace Pacenstein {
                         break;
 
                     case sf::Keyboard::Key::KEY_UP:
+			move("up");
+			break;
                     case sf::Keyboard::Key::KEY_DOWN:
+			move("down");
+			break;
                     case sf::Keyboard::Key::KEY_LEFT:
+			move("left");
+			break;
                     case sf::Keyboard::Key::KEY_RIGHT:
+			move("right");
+			break;
                     case sf::Keyboard::Key::KEY_ALT_UP:
                     case sf::Keyboard::Key::KEY_ALT_DOWN:
                     case sf::Keyboard::Key::KEY_ALT_LEFT:
                     case sf::Keyboard::Key::KEY_ALT_RIGHT:
-                        this->data->machine.addState(state_ref_t(std::make_unique<HuntingState>(this->data)), true);
+                        // this->data->machine.addState(state_ref_t(std::make_unique<HuntingState>(this->data)), true);
+			move("right");
+
                         break;
                 }
             }
         }
     }
 
+    void InGameState::move(std::string direction) {
+	if (direction == "left") {
+            //both camera direction and camera plane must be rotated
+            double oldDirX = this->dirX;
+            this->dirX = this->dirX * cos(rotSpeed) - this->dirY * sin(rotSpeed);
+            this->dirY = oldDirX * sin(rotSpeed) + this->dirY * cos(rotSpeed);
+            double oldPlaneX = this->planeX;
+            this->planeX = this->planeX * cos(rotSpeed) - this->planeY * sin(rotSpeed);
+            this->planeY = oldPlaneX * sin(rotSpeed) + this->planeY * cos(rotSpeed);
+	} else if (direction == "right") {
+            double oldDirX = dirX;
+            this->dirX = this->dirX * cos(-rotSpeed) - this->dirY * sin(-rotSpeed);
+            this->dirY = oldDirX * sin(-rotSpeed) + this->dirY * cos(-rotSpeed);
+            double oldPlaneX = planeX;
+            this->planeX = this->planeX * cos(-rotSpeed) - this->planeY * sin(-rotSpeed);
+            this->planeY = this->oldPlaneX * sin(-rotSpeed) + this->planeY * cos(-rotSpeed);
+
+	} else if (direction == "up") {
+	    if(this->worldMap[int(posX + dirX * moveSpeed)][int(posY)] == false) posX += dirX * moveSpeed;
+	    if(this->worldMap[int(posX)][int(posY + dirY * moveSpeed)] == false) posY += dirY * moveSpeed;
+	} else if (direction == "down") {
+	    if(this->worldMap[int(posX - dirX * moveSpeed)][int(posY)] == false) posX -= dirX * moveSpeed;
+	    if(this->worldMap[int(posX)][int(posY - dirY * moveSpeed)] == false) posY -= dirY * moveSpeed;
+	}
+	std::cout << planeX << '\n';
+    }
     void InGameState::update(float dt) {
-        std::cout << "In game" << std::endl;
+        //std::cout << "In game" << std::endl;
+	std::cout << "posX: " << this->posX << " posY: " << this->posY << '\n';
+	std::cout << "dirX: " << this->dirX << " dirY: " << this->dirY << '\n';
+	std::cout << "planeX: " << this->planeX << " planeY: " << this->planeY << '\n';
+	std::cout << "camera: "<< this->cameraX << '\n';
+
+
+	std::cout << '\n';
+
     }
 
-    // Checkout https://lodev.org/cgtutor/raycasting.html for the original algoritm to generate a 2.5D world
+    // Checkout https://lodev.org/cgtutor/raycasting.html for the original algoritm to generate a 2.5D world.
+    // This is a port of that specific algoritm for SFML.
     void InGameState::draw(float dt) {
 	this->data->window.clear();
 	//std::vector<sf::Vertex> vertices = {
 	// loop door de hele breedte van het scherm
-	double posX = 22, posY = 12;  //x and y start position
-	double dirX = -1, dirY = 0; //initial direction vector
-	double planeX = 0, planeY = 0.66; //the 2d raycaster version of camera plane
-	
-	double time = 0; //time of current frame
-	double oldTime = 0; //time of previous frame
-	
+
+	this->data->window.setFramerateLimit(60);
+	sf::Clock clock = sf::Clock();
+	sf::Time fps;
+
 	for (int x = 0; x < SCREEN_WIDTH; x++) {
 	    //calculate ray position and direction
-	    double cameraX = 2 * x / double(SCREEN_WIDTH) - 1; //x-coordinate in camera space
-	    double rayDirX = dirX + planeX * cameraX;
-	    double rayDirY = dirY + planeY * cameraX;
+	    this->cameraX = 2 * x / double(SCREEN_WIDTH) - 1; //x-coordinate in camera space
+	    double rayDirX = this->dirX + this->planeX * this->cameraX;
+	    double rayDirY = this->dirY + this->planeY * this->cameraX;
 	    
 	    //which box of the map we're in
-	    int mapX = int(posX);
-	    int mapY = int(posY);
+	    int mapX = int(this->posX);
+	    int mapY = int(this->posY);
 	    
 	    //length of ray from current position to next x or y-side
 	    double sideDistX;
@@ -127,16 +173,16 @@ namespace Pacenstein {
 	    //calculate step and initial sideDist
 	    if (rayDirX < 0) {
 		stepX = -1;
-		sideDistX = (posX - mapX) * deltaDistX;
+		sideDistX = (this->posX - mapX) * deltaDistX;
 	    } else {
 		stepX = 1;
-		sideDistX = (mapX + 1.0 - posX) * deltaDistX;
+		sideDistX = (mapX + 1.0 - this->posX) * deltaDistX;
 	    } if (rayDirY < 0) {
 		stepY = -1;
-		sideDistY = (posY - mapY) * deltaDistY;
+		sideDistY = (this->posY - mapY) * deltaDistY;
 	    }  else {
 		stepY = 1;
-		sideDistY = (mapY + 1.0 - posY) * deltaDistY;
+		sideDistY = (mapY + 1.0 - this->posY) * deltaDistY;
 	    }
 	    
 	    //perform DDA
@@ -152,16 +198,16 @@ namespace Pacenstein {
 		    side = 1;
 		}
                 //Check if ray has hit a wall
-		if (worldMap[mapX][mapY] > 0) {
+		if (this->worldMap[mapX][mapY] > 0) {
 		    hit = 1;
 		}
 	    }
 
 	    //Calculate distance projected on camera direction (oblique distance will give fisheye effect!)
 	    if (side == 0) {
-		perpWallDist = fabs((mapX - posX + (1 - stepX) / 2) / rayDirX);
+		perpWallDist = fabs((mapX - this->posX + (1 - stepX) / 2) / rayDirX);
 	    } else {
-		perpWallDist = fabs((mapY - posY + (1 - stepY) / 2) / rayDirY);
+		perpWallDist = fabs((mapY - this->posY + (1 - stepY) / 2) / rayDirY);
 	    }
 	    
 	    //Calculate height of line to draw on screen
@@ -175,7 +221,7 @@ namespace Pacenstein {
 	    
 	    //choose wall color
 	    sf::Color color;
-	    switch(worldMap[mapX][mapY]) {
+	    switch(this->worldMap[mapX][mapY]) {
 	    case 1:  color = sf::Color::Red;    break; //red
             case 2:  color = sf::Color::Green;  break; //green
             case 3:  color = sf::Color::Blue;   break; //blue
@@ -193,6 +239,9 @@ namespace Pacenstein {
 		
 	    };
 	    this->data->window.draw(vertices, 2, sf::Lines);
+
+
+
 	}
 	this->data->window.display();
     }
