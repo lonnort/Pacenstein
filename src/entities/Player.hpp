ff6c63672756f7afc0629c08ca1f185ea266bb20
Lennard Duinkerken <lennarduinkerken@gmail.com>
2022-01-21 12:14:26 +0100

    Dox

--- src/entities/Player.hpp
+++ src/entities/Player.hpp
@@ -6,11 +6,12 @@
 
 namespace Pacenstein {
     /**
+     * The player is an entity that moves around in the map according to the user's input.
      *
+     * Might end up unused.
      */
     class Player : public Entity {
     private:
-    	sf::Vector2f plane;
     	// score Score;
     	unsigned int lives;
     };
--- src/entities/items/Ghost.hpp
+++ src/entities/items/Ghost.hpp
@@ -1,11 +1,15 @@
 #pragma once
 
-#include "../Item.hpp"
+#include "Item.hpp"
 
 namespace Pacenstein {
-    class Ghost : Item {
-    public:
-        Ghost(){}
-        ~Ghost(){}
-    };
+    /**
+     * Ghosts are item that move around on their own volition.
+     *
+     * Every ghost has a different AI for attacking the Player. Every ghost is capable of deducting
+     * a life from the player, unless the player has eaten a PowerPellet, then the ghosts will
+     * scatter and flee from the player. If the player were to eat a ghost in this state points are
+     * awarded based on how many ghosts were eaten: 200 for the first, then 400, 800, and 1600 last.
+     */
+    class Ghost : public Item {};
 }
