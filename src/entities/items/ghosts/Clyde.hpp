703b51ac49cfa7da93949d42a8109750c1627d53
Appenzeill <daanroth@protonmail.com>
2022-01-21 11:58:43 +0100

    Clyde documentation

--- src/entities/items/ghosts/Clyde.hpp
+++ src/entities/items/ghosts/Clyde.hpp
@@ -3,9 +3,24 @@
 #include "../Ghost.hpp"
 
 namespace Pacenstein {
-    class Pinky : Ghost {
+    /**
+     * The subclass for Clyde.
+     *
+     * Clyde is a subclass of Ghost, Clyde is the orange ghost.
+     *
+     * Clyde is the last to leave the house. He only exists the house if a third of the pellets has been eaten.
+     */
+
+    class Clyde : public Ghost {
     public:
-        Pinky(){}
-        ~Pinky(){}
+	/**
+	 * Constructor to initialize Clyde subclass.
+	 */
+        Clyde(){}
+	
+	/**
+	 * Destructor to destruct Clyde subclass.
+	 */
+        ~Clyde(){}
     };
 }
