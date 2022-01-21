43d29ccebb0cfad2e4d95a31a9c6c356bee7a7e8
Appenzeill <daanroth@protonmail.com>
2022-01-21 11:54:00 +0100

    Blinky documentation

--- src/entities/items/ghosts/Blinky.hpp
+++ src/entities/items/ghosts/Blinky.hpp
@@ -3,9 +3,24 @@
 #include "../Ghost.hpp"
 
 namespace Pacenstein {
-    class Blinky : Ghost {
+    /**
+     * The subclass for Blinky.
+     *
+     * Blinky is a subclass of Ghost, Blinky is the red ghost.
+     *
+     * Blinky is the most aggresive ghost, as he tries to make a beeline,
+     * to the player immediately.
+     */
+    class Blinky : public Ghost {
     public:
+	/**
+	 * Constructor to initialize Blinky subclass.
+	 */
         Blinky(){}
+	
+	/**
+	 * Destructor to destruct Blinky subclass.
+	 */
         ~Blinky(){}
     };
 }
