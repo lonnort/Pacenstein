db326e8d7be6ebffc92ba580da3a7dfa2eea6dee
EmmaRaijmakers <71829294+EmmaRaijmakers@users.noreply.github.com>
2022-01-21 10:50:38 +0100

    Added documentation to CreditsMenuState

--- src/states/CreditsMenuState.hpp
+++ src/states/CreditsMenuState.hpp
@@ -5,14 +5,52 @@
 #include "Game.hpp"
 
 namespace Pacenstein {
+	/**
+     * The class for the credits menu state.
+     *
+     * The CreditsMenuState is used to create a state that displays
+	 * information about the creators of the game and shows which
+	 * creator is responsible for a functionalty of the game.
+	 * The credits menu is accessible through the main menu state.
+     */
 	class CreditsMenuState : public State {
 	public:
+		/**
+		 * Constructor to initialize CreditsMenuState class.
+		 * 
+		 * \param data A shared pointer to the game data.
+		 */
 		CreditsMenuState(game_data_ref_t data);
 
+		/**
+		 * Initializes credit menu variables and screen objects.
+		 * 
+		 * Only called once, when the class is created.
+		 */
 		void init();
 
+		/**
+		 * Handles input from player.
+		 * 
+		 * Reacts to mouse input when a sprite is clicked, 
+		 * when a sprite is hovered over and when the window is closed.
+		 */
 		void handleInput();
+
+		/**
+		 * Updates the credit menu state
+		 * 
+		 * Called at a certain time interval
+		 * 
+		 * \param dt The time passed since last update call.
+		 */
 		void update(float dt);
+
+		/**
+		 * Draws the screen objects on the screen.
+		 * 
+		 * \param dt The time passed since last draw call.
+		 */
 		void draw(float dt);
 
 	private:
