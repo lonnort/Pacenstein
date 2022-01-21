fc89d23b47cde9845fb7d7693d7cb8d38804e037
Appenzeill <daanroth@protonmail.com>
2022-01-21 11:41:52 +0100

    Eerste versie van documentatie InGameState.

--- src/states/InGameState.hpp
+++ src/states/InGameState.hpp
@@ -8,28 +8,69 @@
 #include "Definitions.hpp"
 
 namespace Pacenstein {
+    /**
+     * The class for the in game state.
+     *
+     * The InGameState is used to display the current game, via raycasting.
+     * It also manages the movement of the game character. 
+     */
     class InGameState : public State {
     public:
-		InGameState(game_data_ref_t data);
-		void init();
-		void handleInput();
-		void update(float dt);
-		void draw(float dt);
-	
-		void move(std::string direction);
+	/**
+	 * Constructor to initialize InGamestate class.
+	 * 
+	 * \param data A shared pointer to the game data.
+	 */
+	InGameState(game_data_ref_t data);
 
+	/**
+	 * Initializes the variables of the game state.
+	 * 
+	 * Only called once, when the class is created.
+	 */
+	void init();
+
+	/**
+	 * Handles input from the player.
+	 * 
+	 * Reacts to the AWSD and arrow keys to move the character around. 
+	 */
+	void handleInput();
+
+        /**
+         * Updates the in game state outside of key presses.
+         *
+ 	 * \param dt A float.
+	 */
+	void update(float dt);
+	
+        /**
+         * Updates the in game state outside of key presses.
+         *
+ 	 * \param dt A float.
+	 */
+	void draw(float dt);
+
+	
+        /**
+         * Moves the character.
+         *
+ 	 * \param direction A string indicating what direction the player should move.
+	 */
+	void move(std::string direction);
+	
     private:
-		game_data_ref_t data;
+	game_data_ref_t data;
 	
-		sf::Sprite background;
+	sf::Sprite background;
 	
-	        int w, h;
-	        double posX, posY;
-	        double dirX, dirY;
-	        double planeX, planeY;
-	        double moveSpeed;
-	        double rotSpeed;
-	        sf::Clock clock;
-	        sf::Time fps;
+	int w, h;
+	double posX, posY;
+	double dirX, dirY;
+	double planeX, planeY;
+	double moveSpeed;
+	double rotSpeed;
+	sf::Clock clock;
+	sf::Time fps;
     };
 }
