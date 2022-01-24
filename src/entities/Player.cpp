fc1ce45bfaca0b75748de9770ac7ea4c73e81c44
EmmaRaijmakers <71829294+EmmaRaijmakers@users.noreply.github.com>
2022-01-24 12:06:05 +0100

    Added functions to Player class

--- src/entities/Player.cpp
+++ src/entities/Player.cpp
@@ -1,1 +1,47 @@
 #include "Player.hpp"
+
+namespace Pacenstein {
+    Player::Player(){
+        //call entity constructor
+    }
+
+    unsigned int Player::getLives(){
+        return this->lives;
+    }
+    
+    unsigned int Player::getScore(){
+        return this->score;
+    }
+
+    void Player::moveLeft(){
+
+    }
+
+    void Player::moveRight(){
+        
+    }
+
+    void Player::moveUp(){
+        
+    }
+
+    void Player::moveDown(){
+        
+    }
+
+    void Player::addToScore(unsigned int points){
+        this->score += points;
+    }
+
+    void Player::removeLive(){
+        this->lives--;
+    }
+
+    void Player::resetLives(){
+        this->lives = 3;
+    }
+
+    void Player::resetScore(){
+        this->score = 0;
+    }
+}
--- src/entities/Player.hpp
+++ src/entities/Player.hpp
@@ -1,18 +1,71 @@
 #pragma once
 
-#include <SFML/System/Vector2.hpp>
-
 #include "Entity.hpp"
 
 namespace Pacenstein {
     /**
      * The player is an entity that moves around in the map according to the user's input.
      *
-     * Might end up unused.
+     * A player has movement functions to react to input, 
+     * 3 lives and a score.
      */
-    class Player : public Entity {
+    class Player : public Entity{
+    public:
+        Player();
+
+        /**
+         * Returns the number of lives of the player.
+         *
+         * \return An unsigned int of the number of lives.
+         */
+        unsigned int getLives();
+
+        /**
+         * Returns the score of the player.
+         *
+         * \return An unsigned int of the score.
+         */
+        unsigned int getScore();
+
+
+        void moveLeft();
+        void moveRight();
+        void moveUp();
+        void moveDown();
+
+        /**
+         * Adds the points of a fruit/pellet/ghost to the score
+         * of the player.
+         *
+         * \param points An unsigned int to add to the score.
+         */
+        void addToScore(unsigned int points);
+
+        /**
+         * Removes one live of the player.
+         */
+        void removeLive();
+
+        /**
+         * Resets the lives of the player back to the 
+         * original value of 3.
+         * 
+         * Used when a new game is started.
+         */
+        void resetLives();
+
+        /**
+         * Resets the score of the player back to the 
+         * original value of 0.
+         * 
+         * Used when a new game is started.
+         */
+        void resetScore();
+        
+
     private:
-        // score Score;
-        unsigned int lives;
+    	sf::Vector2f plane;
+    	unsigned int lives = 3;
+    	unsigned int score = 0;
     };
 }
