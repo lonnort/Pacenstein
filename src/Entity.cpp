ad825d1d32cf6caa5c14d737f5f3c0b159133071
EmmaRaijmakers <71829294+EmmaRaijmakers@users.noreply.github.com>
2022-01-25 14:37:46 +0100

    Added Entity constructor

--- src/Entity.cpp
+++ src/Entity.cpp
@@ -1,1 +1,9 @@
 #include "Entity.hpp"
+
+namespace Pacenstein{
+    Entity::Entity(sf::Vector2f position, sf::Vector2f direction, float moveSpeed):
+        position(position),
+        direction(direction),
+        moveSpeed(moveSpeed)
+    {}
+}
--- src/Entity.hpp
+++ src/Entity.hpp
@@ -10,10 +10,18 @@ namespace Pacenstein {
      */
     class Entity {
     public:
+        /**
+         * Constructor for the Entity class
+         *
+         * \param position The position of the entity as an sf::Vector2f.
+         * \param direction The direction the entity faces as an sf::Vector2f.
+         * \param moveSpeed The speed with which the entity moves as a float.
+         */
+        Entity(sf::Vector2f position, sf::Vector2f direction, float moveSpeed);
 
-    private:
+    protected:
     	sf::Vector2f position;
     	sf::Vector2f direction;
-    	float move_speed;
+    	float moveSpeed;
     };
 }
