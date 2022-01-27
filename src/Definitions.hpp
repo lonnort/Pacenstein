c940116bb7cd2495ecf70740023515fc495c933b
Lennard Duinkerken <lennarduinkerken@gmail.com>
2022-01-27 18:39:10 +0100

    move the data folder out of src

--- src/Definitions.hpp
+++ src/Definitions.hpp
@@ -7,16 +7,18 @@
 // #define AA_LEVEL      4
 
 // File paths
-#define BACKGROUND_FILEPATH      "res/splash/background.png"
-#define GHOSTS_FILEPATH          "res/sprites/ghosts/"
-#define FONT_FILEPATH            "res/VT323-Regular.ttf"
+#define SETTINGS_FILEPATH        "data/settings.conf"
 #define SCORE_FILEPATH           "src/data/scores.csv"
-#define SETTINGS_FILEPATH        "src/data/settings.conf"
+
 #define BUTTONS_FILEPATH         "res/ui/"
-#define PACENSTEIN_LOGO_FILEPATH "res/pacenstein.png"
-#define PELLET_FILEPATH          "res/sprites/items/pellets/"
 #define GAME_FILEPATH            "res/game/"
+#define GHOSTS_FILEPATH          "res/sprites/ghosts/"
+#define PELLET_FILEPATH          "res/sprites/items/pellets/"
+
 #define MAP_FILEPATH             "res/map.png"
+#define PACENSTEIN_LOGO_FILEPATH "res/pacenstein.png"
+#define BACKGROUND_FILEPATH      "res/splash/background.png"
+#define FONT_FILEPATH            "res/VT323-Regular.ttf"
 
 // Keyboard input keys
 #define KEY_UP        W
