c87c6a64aa4979d13638552e4dccfe4f6afb7713
JarnoBrocker <jarno.brocker@gmail.com>
2022-01-28 15:06:34 +0100

    hijf

--- src/states/InGameState.cpp
+++ src/states/InGameState.cpp
@@ -99,7 +99,7 @@ namespace Pacenstein {
         sf::Sprite WallTexture;
 
         float scaleY;
-        float scaleX;
+        int scaleX;
 
         int LastMapX = -1, LastMapY = -1;
         int LastSide = -1;
@@ -196,11 +196,12 @@ namespace Pacenstein {
             if (scaleX < 1) scaleX = 1;
 
             switch (worldMap[mapX][mapY]) {
-                case 1:  WallTexture.setTexture(doorTexture); break;
-                // case 2:  WallTexture.setTexture(pacTexture); break;
+                case 0:  break;
+                case 1:  WallTexture.setTexture(wallTexture); break;
+                case 2:  break;
                 case 3:  WallTexture.setTexture(wallTexture); break;
-                // case 4:  WallTexture.setTexture(pacTexture); break;
-                // default: WallTexture.setTexture(wallTexture); break;
+                case 4:  break;
+                default: WallTexture.setTexture(wallTexture); break;
             }
 
             if (LastMapY == mapY && LastMapX == mapX && side == LastSide) Repeats++;
@@ -214,7 +215,7 @@ namespace Pacenstein {
                 textureSectionX = 64 - textureSectionX;
             }
 
-            float y = 540 - (64 * scaleY) / 2;
+            float y = 288 - (64 * scaleY) / 2;
 
             //Draw the walls
             WallTexture.setScale(1, scaleY);
@@ -378,8 +379,8 @@ namespace Pacenstein {
 
         std::vector<Sprite> sprites = {
             {1.5, 3.5, blinkyTexture},
-            // {1.5, 5.5, blinkyTexture},
-            // {1.5, 7.5, blinkyTexture},
+            {1.5, 5.5, blinkyTexture},
+            {1.5, 7.5, blinkyTexture},
         };
 
         sf::Vector2f position  = player.getPos();
@@ -392,8 +393,8 @@ namespace Pacenstein {
         this->fps = this->clock.getElapsedTime();
         this->clock.restart();
 
-        player.setMoveSpeed(fps.asSeconds() * 10.0); //the constant value is in squares/second
-        player.setRotSpeed (fps.asSeconds() * 10.0); //the constant value is in radians/second
+        player.setMoveSpeed(fps.asSeconds() * 20.0); //the constant value is in squares/second
+        player.setRotSpeed (fps.asSeconds() * 20.0); //the constant value is in radians/second
         
         sf::Text scoreText("Score: " + std::to_string(this->data->score), this->data->assets.getFont("Font"));
         scoreText.setPosition(5, 5);
