a607608b745a0342d7cb3665b3c98d9e4586fee3
EmmaRaijmakers <71829294+EmmaRaijmakers@users.noreply.github.com>
2022-01-31 11:03:17 +0100

    Added constructors to Cherry class

--- src/entities/items/fruits/cherry.cpp
+++ src/entities/items/fruits/cherry.cpp
@@ -1,1 +1,12 @@
 #include "cherry.hpp"
+
+namespace Pacenstein {
+    Cherry::Cherry(float x, float y):
+        Fruit(x, y, 100)
+    {}
+
+    Cherry::Cherry(sf::Vector2f xy):
+        Cherry(xy.x, xy.y)
+    {}
+}
+
--- src/entities/items/fruits/cherry.hpp
+++ src/entities/items/fruits/cherry.hpp
@@ -6,5 +6,22 @@ namespace Pacenstein {
     /**
      * Cherries are a type of fruit and give 100 points.
      */
-    class Cherry : public Fruit {};
+    class Cherry : public Fruit {
+    public:
+        /**
+         * Constructor of the Cherry class.
+         *
+         * \param x The x position as a float of a cherry.
+         * \param y The y position as a float of a cherry.
+         */
+        Cherry(float x, float y);
+
+        /**
+         * Constructor of the Cherry class.
+         *
+         * \param xy The position of a cherry as a sf::Vector2f.
+         */
+        explicit Cherry(sf::Vector2f xy);
+
+    };
 }
