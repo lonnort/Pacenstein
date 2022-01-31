cd6881ae033ed0ca8d29215800ab8fe8fb2aca4d
EmmaRaijmakers <71829294+EmmaRaijmakers@users.noreply.github.com>
2022-01-31 11:34:51 +0100

    Added constructors to Peach class

--- src/entities/items/fruits/peach.cpp
+++ src/entities/items/fruits/peach.cpp
@@ -1,1 +1,11 @@
 #include "peach.hpp"
+
+namespace Pacenstein {
+    Peach::Peach(float x, float y):
+        Fruit(x, y, 500)
+    {}
+
+    Peach::Peach(sf::Vector2f xy):
+        Peach(xy.x, xy.y)
+    {}
+}
--- src/entities/items/fruits/peach.hpp
+++ src/entities/items/fruits/peach.hpp
@@ -6,5 +6,22 @@ namespace Pacenstein {
     /**
      * Peaches (Oranges according to some) are a type of fruit and give 500 points.
      */
-    class Peach : public Fruit {};
+    class Peach : public Fruit {
+    public:
+        /**
+         * Constructor of the Peach class.
+         *
+         * \param x The x position as a float of a peach.
+         * \param y The y position as a float of a peach.
+         */
+        Peach(float x, float y);
+
+        /**
+         * Constructor of the Peach class.
+         *
+         * \param xy The position of a peach as a sf::Vector2f.
+         */
+        explicit Peach(sf::Vector2f xy);
+
+    };
 }
