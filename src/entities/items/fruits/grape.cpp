ae06b92f460a87bed71811cda230c9565a216d30
EmmaRaijmakers <71829294+EmmaRaijmakers@users.noreply.github.com>
2022-01-31 11:13:01 +0100

    Added constructors to Grape class

--- src/entities/items/fruits/grape.cpp
+++ src/entities/items/fruits/grape.cpp
@@ -1,1 +1,11 @@
 #include "grape.hpp"
+
+namespace Pacenstein {
+    Grape::Grape(float x, float y):
+        Fruit(x, y, 1000)
+    {}
+
+    Grape::Grape(sf::Vector2f xy):
+        Grape(xy.x, xy.y)
+    {}
+}
--- src/entities/items/fruits/grape.hpp
+++ src/entities/items/fruits/grape.hpp
@@ -6,5 +6,22 @@ namespace Pacenstein {
     /**
      * Grapes (Melons according to some) are a type of fruit and give 1000 points.
      */
-    class Grape : public Fruit {};
+    class Grape : public Fruit {
+    public:
+        /**
+         * Constructor of the Grape class.
+         *
+         * \param x The x position as a float of a grape.
+         * \param y The y position as a float of a grape.
+         */
+        Grape(float x, float y);
+
+        /**
+         * Constructor of the Grape class.
+         *
+         * \param xy The position of a grape as a sf::Vector2f.
+         */
+        explicit Grape(sf::Vector2f xy);
+
+    };
 }
