db77ecfd7ce05a5c231bd4fd4b8ccba1b0b36c30
EmmaRaijmakers <71829294+EmmaRaijmakers@users.noreply.github.com>
2022-01-31 11:46:48 +0100

    Added constructors to Strawberry class

--- src/entities/items/fruits/strawberry.cpp
+++ src/entities/items/fruits/strawberry.cpp
@@ -1,1 +1,11 @@
 #include "strawberry.hpp"
+
+namespace Pacenstein {
+    Strawberry::Strawberry(float x, float y):
+        Fruit(x, y, 300)
+    {}
+
+    Strawberry::Strawberry(sf::Vector2f xy):
+        Strawberry(xy.x, xy.y)
+    {}
+}
--- src/entities/items/fruits/strawberry.hpp
+++ src/entities/items/fruits/strawberry.hpp
@@ -6,5 +6,22 @@ namespace Pacenstein {
     /**
      * Strawberries are a type of fruit and give 300 points.
      */
-    class Strawberry : public Fruit {};
+    class Strawberry : public Fruit {
+    public:
+        /**
+         * Constructor of the Strawberry class.
+         *
+         * \param x The x position as a float of a strawberry.
+         * \param y The y position as a float of a strawberry.
+         */
+        Strawberry(float x, float y);
+
+        /**
+         * Constructor of the Strawberry class.
+         *
+         * \param xy The position of a strawberry as a sf::Vector2f.
+         */
+        explicit Strawberry(sf::Vector2f xy);
+
+    };
 }
