3890916af5236f8ae17e42087d25e867ec1ac22e
EmmaRaijmakers <71829294+EmmaRaijmakers@users.noreply.github.com>
2022-01-31 10:50:34 +0100

    Added constructors to Apple class

--- src/entities/items/fruits/apple.cpp
+++ src/entities/items/fruits/apple.cpp
@@ -1,1 +1,11 @@
 #include "apple.hpp"
+
+namespace Pacenstein {
+    Apple::Apple(float x, float y):
+        Fruit(x, y, 700)
+    {}
+
+    Apple::Apple(sf::Vector2f xy):
+        Apple(xy.x, xy.y)
+    {}
+}
--- src/entities/items/fruits/apple.hpp
+++ src/entities/items/fruits/apple.hpp
@@ -6,5 +6,22 @@ namespace Pacenstein {
     /**
      * Apples are a type of fruit and give 700 points.
      */
-    class Apple : public Fruit {};
+    class Apple : public Fruit {
+    public:
+        /**
+         * Constructor of the Apple class.
+         *
+         * \param x The x position as a float of an apple.
+         * \param y The y position as a float of an apple.
+         */
+        Apple(float x, float y);
+
+        /**
+         * Constructor of the Apple class.
+         *
+         * \param xy The position of an apple as a sf::Vector2f.
+         */
+        explicit Apple(sf::Vector2f xy);
+
+    };
 }
