a1c22fdeff230e972e49e4b535da62d077e1336c
EmmaRaijmakers <71829294+EmmaRaijmakers@users.noreply.github.com>
2022-02-01 23:29:32 +0100

    Fixed move function in Blinky class

--- src/entities/items/ghosts/Blinky.cpp
+++ src/entities/items/ghosts/Blinky.cpp
@@ -34,49 +34,41 @@ namespace Pacenstein {
         switch (this->direction){
             case 1: // Noord
                 if((this->position.y - int(this->position.y)) < 0.5){
-                    if(worldMap[int(this->position.y) - 1][int(this->position.x)] == 1) { //if block above ghost is a wall
+                    if(worldMap[int(this->position.x)][int(this->position.y) - 1] == 3) { //if block above ghost is a wall
                         this->direction = rand() % 4 + 1;
-                        // std::cout << "direction x: " << this->direction.x << " direction y: " << this->direction.y;
                         break;
                     }
                 }
-                // std::cout << this->getPos().y << "\n";
                 this->position = {this->position.x, this->position.y - movement_speed};
                 break;
 
             case 2: // Oost
                 if((this->position.x - int(this->position.x)) > 0.5){
-                    if(worldMap[int(this->position.y)][int(this->position.x) + 1] == 1) { //if block right of ghost is a wall
+                    if(worldMap[int(this->position.x) + 1][int(this->position.y)] == 3) { //if block right of ghost is a wall
                         this->direction = rand() % 4 + 1;
-                        // std::cout << "direction x: " << this->direction.x << " direction y: " << this->direction.y;
                         break;
                     }
                 }
-                // std::cout << this->getPos().y << "\n";
                 this->position = {this->position.x + movement_speed, this->position.y};
                 break;
 
             case 3: // Zuid
                 if((this->position.y - int(this->position.y)) > 0.5){
-                    if(worldMap[int(this->position.y) + 1][int(this->position.x)] == 1) { //if block below ghost is a wall
+                    if(worldMap[int(this->position.x)][int(this->position.y) + 1] == 3) { //if block below ghost is a wall
                         this->direction = rand() % 4 + 1;
-                        // std::cout << "direction x: " << this->direction.x << " direction y: " << this->direction.y;
                         break;
                     }
                 }
-                // std::cout << this->getPos().y << "\n";
                 this->position = {this->position.x, this->position.y + movement_speed};
                 break;
 
             case 4: // West
                 if((this->position.x - int(this->position.x)) < 0.5){
-                    if(worldMap[int(this->position.y)][int(this->position.x) - 1] == 1) { //if block left of ghost is a wall
+                    if(worldMap[int(this->position.x) - 1][int(this->position.y)] == 3) { //if block left of ghost is a wall
                         this->direction = rand() % 4 + 1;
-                        // std::cout << "direction x: " << this->direction.x << " direction y: " << this->direction.y;
                         break;
                     }
                 }
-                // std::cout << this->getPos().y << "\n";
                 this->position = {this->position.x - movement_speed, this->position.y};
                 break;
         }
